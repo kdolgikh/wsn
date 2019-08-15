23a3c2c365a89b0fc277da648ddcc4fa7b8e42d0
kdolgikh <kdolgikh@outlook.com>
2019-08-14 23:14:37 -0400

    created a project

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,107 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../CH_Sense.c \
+../CH_TDMA_Assign.c \
+../Clustering.c \
+../MSP_Init.c \
+../Message_Prep.c \
+../ND_Sense.c \
+../Radio.c \
+../RandomGen.c \
+../SPI_Library.c \
+../TDMA_Assign.c \
+../UART.c \
+../Wait_For_Start.c \
+../main.c \
+../sleep_timer.c 
+
+C_DEPS += \
+./CH_Sense.d \
+./CH_TDMA_Assign.d \
+./Clustering.d \
+./MSP_Init.d \
+./Message_Prep.d \
+./ND_Sense.d \
+./Radio.d \
+./RandomGen.d \
+./SPI_Library.d \
+./TDMA_Assign.d \
+./UART.d \
+./Wait_For_Start.d \
+./main.d \
+./sleep_timer.d 
+
+OBJS += \
+./CH_Sense.obj \
+./CH_TDMA_Assign.obj \
+./Clustering.obj \
+./MSP_Init.obj \
+./Message_Prep.obj \
+./ND_Sense.obj \
+./Radio.obj \
+./RandomGen.obj \
+./SPI_Library.obj \
+./TDMA_Assign.obj \
+./UART.obj \
+./Wait_For_Start.obj \
+./main.obj \
+./sleep_timer.obj 
+
+OBJS__QUOTED += \
+"CH_Sense.obj" \
+"CH_TDMA_Assign.obj" \
+"Clustering.obj" \
+"MSP_Init.obj" \
+"Message_Prep.obj" \
+"ND_Sense.obj" \
+"Radio.obj" \
+"RandomGen.obj" \
+"SPI_Library.obj" \
+"TDMA_Assign.obj" \
+"UART.obj" \
+"Wait_For_Start.obj" \
+"main.obj" \
+"sleep_timer.obj" 
+
+C_DEPS__QUOTED += \
+"CH_Sense.d" \
+"CH_TDMA_Assign.d" \
+"Clustering.d" \
+"MSP_Init.d" \
+"Message_Prep.d" \
+"ND_Sense.d" \
+"Radio.d" \
+"RandomGen.d" \
+"SPI_Library.d" \
+"TDMA_Assign.d" \
+"UART.d" \
+"Wait_For_Start.d" \
+"main.d" \
+"sleep_timer.d" 
+
+C_SRCS__QUOTED += \
+"../CH_Sense.c" \
+"../CH_TDMA_Assign.c" \
+"../Clustering.c" \
+"../MSP_Init.c" \
+"../Message_Prep.c" \
+"../ND_Sense.c" \
+"../Radio.c" \
+"../RandomGen.c" \
+"../SPI_Library.c" \
+"../TDMA_Assign.c" \
+"../UART.c" \
+"../Wait_For_Start.c" \
+"../main.c" \
+"../sleep_timer.c" 
+
+
